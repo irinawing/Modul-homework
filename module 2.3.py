49e5d6fe6e082e9ff81e88045e4712514f79dfdf
irinawing <irina.wing@mail.ru>
2024-06-17 16:09:19 +0300

    Практическое задание 2.3

--- module 2.3.py
+++ module 2.3.py
@@ -1,2 +1,15 @@
-if 1 < 2:
-    print("yes")
+my_list = my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+list_len = len(my_list)
+number_repeat = list_len - 1
+
+i = 0
+value = my_list[i]
+
+while value >= 0:
+    print(value)
+    if i < number_repeat:
+        i = i + 1
+        value = my_list[i]
+    else:
+        break
