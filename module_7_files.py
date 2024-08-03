b3c76cd2f949fd18180afa4855408510d8c0f63c
irinawing <irina.wing@mail.ru>
2024-08-04 00:11:56 +0300

    Module_7 os files

---
+++ module_7_files.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+directory = os.getcwd()
+# print(directory)
+
+for root, dirs, files in os.walk(directory):
+  for file in files:
+      filepath = os.path.join(directory, file)
+      filetime = os.path.getmtime(file)
+      formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+      filesize = os.path.getsize(file)
+      parent_dir = os.path.dirname(file)
+      print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
