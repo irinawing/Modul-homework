9c5ec46399ee18283e8829899496358a5dc71b73
irinawing <irina.wing@mail.ru>
2024-09-13 23:56:00 +0200

    Module_11 Интроспекция

---
+++ modul_11_intro.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+
+def introspection_info(obj):
+    type_ = type(obj).__name__
+    attribute = getattr(obj, '__dict__', None)
+    methods = dir(obj)
+    module = obj.__class__.__module__
+    info = {'type': type_, 'attributes': attribute, 'methods': methods, 'module': module}
+    return info
+
+number_info = introspection_info(42)
+print(number_info)
+pprint(number_info)
