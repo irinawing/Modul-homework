3cb33cf73406aa871e1b1a42f532aa8fc81f7c2c
irinawing <irina.wing@mail.ru>
2024-09-28 20:11:24 +0200

    Module_11 Интроспекция, ще раз исправлено 'attributes'

--- modul_11_intro.py
+++ modul_11_intro.py
@@ -2,12 +2,13 @@ from pprint import pprint
 
 def introspection_info(obj):
     type_ = type(obj).__name__
-    attribute = getattr(obj, '__dir__')
+    # attribute = getattr(obj, '__dir__')
+    attribute = [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
     methods = dir(obj)
     module = obj.__class__.__module__
     info = {'type': type_, 'attributes': attribute, 'methods': methods, 'module': module}
     return info
 
 number_info = introspection_info(42)
-print(number_info)
+# print(number_info)
 pprint(number_info)
