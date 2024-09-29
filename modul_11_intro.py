1ddb52f37b2c3a56f8ce069fe514881f40658f14
irinawing <irina.wing@mail.ru>
2024-09-29 14:32:27 +0200

    Module_11_intro переделаны методы

--- modul_11_intro.py
+++ modul_11_intro.py
@@ -3,12 +3,12 @@ from pprint import pprint
 def introspection_info(obj):
     type_ = type(obj).__name__
     # attribute = getattr(obj, '__dir__')
-    attribute = [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
-    methods = dir(obj)
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
+    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
     module = obj.__class__.__module__
-    info = {'type': type_, 'attributes': attribute, 'methods': methods, 'module': module}
+    info = {'type': type_, 'attributes': attributes, 'methods': methods, 'module': module}
     return info
 
 number_info = introspection_info(42)
-print(number_info)
+# print(number_info)
 pprint(number_info)
