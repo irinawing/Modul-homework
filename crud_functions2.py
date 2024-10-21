380f35603b4b9b8cda426c750d6c8380d8ed17b5
irinawing <irina.wing@mail.ru>
2024-10-22 00:35:15 +0200

    Module_14_5 "Написание примитивной ORM"

---
+++ crud_functions2.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    );
+    ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    );
+    ''')
+
+def add_user(username, email, age):
+    cursor.execute(f"INSERT INTO Users (username, email, age, balance) VALUES ('{username}', '{email}', '{age}', 1000)")
+    connection.commit()
+
+
+def is_included(username):
+    user = cursor.execute(f"SELECT * FROM Users WHERE username = ?", (username,))
+    if user.fetchone() is None:
+        return True
+    else:
+        return False
+
+
+def get_all_products():
+    products = cursor.execute("SELECT title, description, price FROM Products")
+    a = []
+    for i in products:
+        a += {f'Название: {i[0]} | Описание: {i[1]} | Цена: {i[2]}'}
+    connection.commit()
+    return a
